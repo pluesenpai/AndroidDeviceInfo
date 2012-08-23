dc0e24aa5cab5474a9a073fd2357e0500142e327
Paolo Cortis <paolo.cortis@gmail.com>
2012-08-23 16:02:35 +0200

    showing screen info

--- src/org/slackcnt/android/androiddeviceinfo/MainActivity.java
+++ src/org/slackcnt/android/androiddeviceinfo/MainActivity.java
@@ -1,24 +1,87 @@
 package org.slackcnt.android.androiddeviceinfo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		TextView textView = (TextView) findViewById(R.id.info_label);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		StringBuilder stringBuilder = new StringBuilder();
 
-    
+		stringBuilder.append("Width: ");
+		stringBuilder.append(dm.widthPixels);
+		stringBuilder.append(" px\n");
+
+		stringBuilder.append("Height: ");
+		stringBuilder.append(dm.heightPixels);
+		stringBuilder.append(" px\n");
+
+		stringBuilder.append("\n");
+
+		stringBuilder.append("Density: ");
+		stringBuilder.append(dm.densityDpi);
+		stringBuilder.append(" dpi\n");
+
+		stringBuilder.append("Density: ");
+		switch(dm.densityDpi) {
+			case DisplayMetrics.DENSITY_LOW:
+				stringBuilder.append("ldpi");
+				break;
+			case DisplayMetrics.DENSITY_MEDIUM:
+				stringBuilder.append("mdpi");
+				break;
+			case DisplayMetrics.DENSITY_HIGH:
+				stringBuilder.append("hdpi");
+				break;
+			case DisplayMetrics.DENSITY_XHIGH:
+				stringBuilder.append("xhdpi");
+				break;
+			case DisplayMetrics.DENSITY_XXHIGH:
+				stringBuilder.append("xxhdpi");
+				break;
+			case DisplayMetrics.DENSITY_TV:
+				stringBuilder.append("tvdpi");
+				break;
+		}
+		stringBuilder.append("\n");
+		stringBuilder.append("\n");
+
+		stringBuilder.append("Screen: ");
+		if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE) {
+			stringBuilder.append("large");
+		} else if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+			stringBuilder.append("normal");
+		} else if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
+			stringBuilder.append("small");
+		} else if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+			stringBuilder.append("xlarge");
+		} else if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_UNDEFINED) {
+			stringBuilder.append("undefined");
+		} else {
+			stringBuilder.append("?");
+		}
+		stringBuilder.append("\n");
+
+		textView.setText(stringBuilder.toString());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
